6b0a23df21229ebc500c4f7ee5872ddc39b14208
Johnny Yip <jyippy2012@gmail.com>
2020-08-25 11:50:35 +0000

    Exercises 8.1 to 8.7 and 8.9

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -5,31 +5,70 @@ def input_students
   students = []
   # get the first name
   name = gets.chomp
+  #Exercise 8.7
+  puts "Enter cohort"
+  cohort = gets.chomp.to_sym
+  #Exercise 8.5
+  puts "Enter hobbies"
+  hobbies = gets.chomp
+  puts "Enter country of birth"
+  country = gets.chomp
   #while the name is not empty, repeat this code
-  while !name.empty? do
+  while !name.empty? && !cohort.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    students << {name: name, cohort: cohort, hobbies: hobbies, country: country}
+      #Exercise 8.9
+      if students.count == 1
+    puts "Now we have #{students.count} student"
+      else
+      puts "Now we have #{students.count} students"
+      end
     # get another name from the user
     name = gets.chomp
+      puts "Enter cohort"
+    cohort = gets.chomp.to_sym
+      puts "Enter hobbies"
+    hobbies = gets.chomp
+      puts "Enter country of birth"
+    country = gets.chomp
   end
   #return the array of students
   students
 end
-  
+
+#Exercise 8.6  
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
 end
 
+#Exercise 8.1, 8.4
 def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index+1} #{student[:name]} (#{student[:cohort]} cohort)"
+  counter = 0
+  while counter < students.count  
+    puts "#{counter+1} #{students[counter][:name]} (#{students[counter][:cohort]} cohort)".center(50)
+    counter += 1
+  end
+end
+
+#Exercise 8.2
+def print_specific_letter(students)
+  puts "User enters a letter to filter"
+  letter = gets.chomp
+  students.select do |student|
+    puts student[:name] if student[:name].start_with?(letter)
+  end
+end
+
+#Exercise 8.3
+def print_less(students)
+   students.select do |student|
+    puts student[:name] if student[:name].length < 12
   end
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(50)
 end
 
 #nothing happens until we call the methods
